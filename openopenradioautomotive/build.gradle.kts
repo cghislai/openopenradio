ef08b9e1c784b52624a1aae0fad2d1a8b0ccb3c0
cghislai <charlyghislain@gmail.com>
2026-01-04 11:57:45 +0100

    mobile v8 automotive v7

--- openopenradioautomotive/build.gradle.kts
+++ openopenradioautomotive/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 }
 
 android {
-    namespace = "com.charlyghislain.openopenradioautomotive"
+    namespace = "com.charlyghislain.openopenradio.automotive"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.charlyghislain.openopenradioautomotive"
+        applicationId = "com.charlyghislain.openopenradio"
         minSdk = 28
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
